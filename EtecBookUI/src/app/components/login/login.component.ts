e9398828cd4df5cd8b4772c3c405d0a36d0fe9a6
Adrian José Pereira Santos <adrianjpsantoos@gmail.com>
2023-05-22 20:35:13 -0300

    criado formularios

--- EtecBookUI/src/app/components/login/login.component.ts
+++ EtecBookUI/src/app/components/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email:['',Validators.required],
+      password:['',Validators.required]
+    });
+  }
 }
