f46d1895ffc6313f397cf7a74019318ddd12d734
Adrian José Pereira Santos <adrianjpsantoos@gmail.com>
2023-05-25 20:39:33 -0300

    Validators Adicionados

--- EtecBookUI/src/app/components/login/login.component.ts
+++ EtecBookUI/src/app/components/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,8 +18,43 @@ export class LoginComponent {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email:['',Validators.required],
-      password:['',Validators.required]
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  checkEmail(): boolean {
+    return (
+      this.loginForm.controls['email'].dirty &&
+      this.loginForm.hasError('required', 'email')
+    );
+  }
+
+  checkPassword(): boolean {
+    return (
+      this.loginForm.controls['password'].dirty &&
+      this.loginForm.hasError('required', 'password')
+    );
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      //Enviar dados
+      console.log(this.loginForm.value);
+    } else {
+      //Dispara Erro
+      this.validateAllFormFields(this.loginForm)
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
     });
   }
 }
--- EtecBookUI/src/app/components/register/register.component.ts
+++ EtecBookUI/src/app/components/register/register.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required,Validators.email])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+      ])],
+    });
+  }
+
+  checkName(): boolean {
+    return (
+      this.registerForm.controls['name'].dirty &&
+      this.registerForm.controls['name'].errors?.['required']
+    );
+  }
+
+  checkEmail(): boolean {
+    return (
+      this.registerForm.controls['email'].dirty &&
+      this.registerForm.controls['email'].errors?.['required']
+    );
+  }
+
+
+  checkEmailValid(): boolean {
+    return (
+      this.registerForm.controls['email'].dirty &&
+      this.registerForm.controls['email'].errors?.['email']
+    );
+  }
+
+  checkPassword(): boolean {
+    return (
+      this.registerForm.controls['password'].dirty &&
+      this.registerForm.controls['password'].errors?.['required']
+    );
+  }
+
+  checkPasswordLength(): boolean {
+    return (
+      this.registerForm.controls['password'].dirty &&
+      this.registerForm.controls['password'].errors?.['minlength']
+    );
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      //Enviar dados
+      console.log(this.registerForm.value);
+    } else {
+      //Dispara Erro
+      this.validateAllFormFields(this.registerForm);
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
 }
